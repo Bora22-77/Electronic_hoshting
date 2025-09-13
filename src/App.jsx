b7db984b8b7f1a66cbee70006c48b986d5983535
Bora22-77 <boyra1took@gmail.com>
2025-09-13 11:15:07 +0700

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Product from './Pages/Product'
+import Contact from './Pages/Contact'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <div className="text-7xl text-blue-800 text-bold">
-        <h1>Welcome Back</h1>
-      </div>
+      <nav>
+        <Navbar/>
+      </nav>
+      <main>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/product' element={<Product/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </main>
+      <footer></footer>
     </div>
   )
 }
